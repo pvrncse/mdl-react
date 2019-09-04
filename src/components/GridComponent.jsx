7ab6fe8ecd4d3ca4f5ea8e5556e6bdf2b611081f
Palthya Venkataramana Naik <nv.palthya@globaledgesoft.com>
2019-09-04 12:55:40 +0530

    dynamic images in dashboard

--- src/components/GridComponent.jsx
+++ src/components/GridComponent.jsx
@@ -1,10 +1,16 @@
 import React, { Component } from "react";
 class GridComponent extends Component {
   state = {};
+  headerStyle = {
+    background:
+      "url(https://via.placeholder.com/728x90.png?text=" +
+      this.props.value.name +
+      ") center / cover"
+  };
   render() {
     return (
       <div className="demo-card-wide mdl-card mdl-shadow--2dp mdl-cell mdl-cell--6-col">
-        <div className="mdl-card__title">
+        <div className="mdl-card__title" style={this.headerStyle}>
           <h2 className="mdl-card__title-text">{this.props.value.name}</h2>
         </div>
         <div className="mdl-card__supporting-text">
@@ -16,9 +22,15 @@ class GridComponent extends Component {
           </a>
         </div>
         <div className="mdl-card__menu">
-          <button className="mdl-button mdl-button--icon mdl-js-button mdl-js-ripple-effect">
-            <i className="material-icons">share</i>
+          <button
+            className="mdl-button mdl-button--icon mdl-js-button mdl-js-ripple-effect"
+            id={this.props.value.name}
+          >
+            <i className="material-icons">remove_red_eye</i>
           </button>
+          <div class="mdl-tooltip" data-mdl-for={this.props.value.name}>
+            View Conversation
+          </div>
         </div>
       </div>
     );
